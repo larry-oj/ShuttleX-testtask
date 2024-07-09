4f805bdab7d3dc98a00f2c661c548a89b52941df
Larry <66905431+larry-oj@users.noreply.github.com>
2024-07-09 19:13:21 +0300

    register repositories in DI container

--- src/ChatShuttleX/Program.cs
+++ src/ChatShuttleX/Program.cs
@@ -1,4 +1,5 @@
 using ChatShuttleX.Data;
+using ChatShuttleX.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,10 @@ builder.Services.AddDbContext<ChatContext>(ops =>
 {
     ops.UseNpgsql(builder.Configuration.GetSection("Database:ConnectionString").Value);
 });
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IChatroomRepository, ChatroomRepository>();
+builder.Services.AddScoped<IChatUserRepository, ChatUserRepository>();
 
 var app = builder.Build();
 
